7629c2947daf88ceb1f3c9ba947f2bba703272fc
MoniNog <nogueiramon@gmail.com>
2024-09-15 18:25:35 +0200

    MAJ cest deja mieux...

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: moni <moni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 11:19:34 by moni              #+#    #+#             */
-/*   Updated: 2024/09/14 21:23:51 by moni             ###   ########.fr       */
+/*   Updated: 2024/09/15 18:13:08 by moni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,33 +30,30 @@ int	main(int ac, char **av)
 	// ./pushswap 1 5 2 3
 	// ./pushswap "1 5 2   	 3"
 	int i = 0;
-	// if (ac > 1 && checks_are_ok(av[1]) == true)
-	if (ac < 2)
-		return (0);
-	if (ac == 2) {
-		z = split(av[1], ' ');
-		while (z[i])
+	if (checks_are_ok(av[1]) == true && ac > 1)
+	{
+		if (ac == 2)
 		{
-			printf("|%s|\n", z[i]);
+			z = split(av[1], ' ');
+			while (z[i])
+			{
+				printf("|%s|\n", z[i]);
+				i++;
+			}
 			i++;
 		}
-		i++;
-	} else {
-		z = (av + 1); // ^= &av[i]
-		printf("%d", ft_strlen(z));
-		i = ac;
+		create_stack(&stack_a, &stack_b, &info, ac, av);// !! ici pour les arg
+		print_stack(stack_a, stack_b, info);
+		choose_sort(&stack_a, &stack_b, info);
+		print_stack(stack_a, stack_b, info);
+		print_move_and_size(stack_a, stack_b, info);
+
+		free(info);
+		free_stack(stack_a);
 	}
-
-	create_stack(&stack_a, &stack_b, &info, ac, z);// !! ici pour les arg
-	print_stack(stack_a, stack_b, info);
-	choose_sort(&stack_a, &stack_b, info);
-	print_stack(stack_a, stack_b, info);
-	print_move_and_size(stack_a, stack_b, info);
-	//	implicitement, si pas d'argument ou un seul argument
-	//	il ne se passe rien.
-
-	free(info);
-	free_stack(stack_a);
-
 	return (0);
 }
+
+
+		//	implicitement, si pas d'argument ou un seul argument
+		//	il ne se passe rien.
--- src/stack/print_stack.c
+++ src/stack/print_stack.c
@@ -6,7 +6,7 @@
 /*   By: moni <moni@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 19:52:37 by moni              #+#    #+#             */
-/*   Updated: 2024/09/14 19:52:11 by moni             ###   ########.fr       */
+/*   Updated: 2024/09/15 18:24:54 by moni             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 #define RED "\033[1;91m"
 #define BLUE "\033[1;34m"
+#define RESET "\033[0m"
 
 void	print_stack(t_stack* stack_a, t_stack* stack_b, t_info* info)
 {
@@ -21,54 +22,61 @@ void	print_stack(t_stack* stack_a, t_stack* stack_b, t_info* info)
 
 	index = 0;
 
+	// Vérifie si la pile B est vide
 	if (!stack_b)
 	{
 		printf(BLUE "\n\t\t\t┌───────────────┐\n");
 		printf("\t\t\t│ Empty stack_b │\n");
 		printf("\t\t\t└───────────────┘\n");
-		printf("\t\t\t  * ** *** ** *  \033[0m\n");
+		printf("\t\t\t  * ** *** ** *  " RESET "\n");
 	}
 
+	// Affichage principal
 	printf(RED "\n\n─────────────────────────────────────────────────────────" BLUE "\n\n");
 	printf(BLUE "┌───────────────┐\t┌───────────────┐\n");
 	printf("│    Stack A    │\t│    Stack B    │\n");
 	printf("└───────────────┘\t└───────────────┘\n");
 	printf("┌─────┬─────────┐\t┌─────┬─────────┐\n");
-	printf("│" RED " Ind " BLUE "│"  RED "  Value"  BLUE "|\t│" RED " Ind " BLUE "│" "  Value  " BLUE "│\n");
-	printf("├─────┼─────────┤\t├─────┼─────────┤\n\033[0m");
+	printf("│" RED " Ind " BLUE "│"  RED "  Value  "  BLUE "│\t│" RED " Ind " BLUE "│" "  Value  " BLUE "│\n");
+	printf("├─────┼─────────┤\t├─────┼─────────┤\n" RESET);
 
-	while (index < 10)
+	// Affiche les éléments des piles tant qu'ils existent
+	while (index < info->size)
 	{
 		if (stack_a)
 		{
-			printf(BLUE "│ " RED "%2d  " BLUE "│ " RED "%5d" BLUE " │\t", index, stack_a->content);
+			// Affichage bien aligné pour Stack A
+			printf(BLUE "│ " RED "%2d  " BLUE "│ " RED "%5d\t" BLUE "│\t", index, stack_a->content);
 			stack_a = stack_a->next;
 		}
 		else
+			// Si Stack A est vide, on affiche des espaces pour garder l'alignement
 			printf(BLUE "│     │         │\t");
 
 		if (stack_b)
 		{
+			// Affichage bien aligné pour Stack B
 			printf("│ " RED "%2d  " BLUE "│ " RED "%5d" BLUE " │\n", index, stack_b->content);
 			stack_b = stack_b->next;
 		}
 		else
+			// Si Stack B est vide, on affiche des espaces pour garder l'alignement
 			printf("│     │         │\n");
 
 		index++;
 	}
 	
-	printf("└─────┴─────────┘\t└─────┴─────────┘\033[0m\n\n");
-	printf(RED "─────────────────────────────────────────────────────────" RED "\n\n");
+	printf("└─────┴─────────┘\t└─────┴─────────┘" RESET "\n\n");
+	printf(RED "─────────────────────────────────────────────────────────" RESET "\n\n");
 }
 
 void	print_move_and_size(t_stack* stack_a, t_stack* stack_b, t_info* info)
 {
 	printf("\t" BLUE "┌─────────────────────────┐\n");
-	printf("\t│" RED " Count move : %2i" BLUE "    │\n", info->move);
+	printf("\t│" RED "    Count move : %2i" BLUE "      │\n", info->move);
 	printf("\t└─────────────────────────┘\n");
 
 	printf("\n\t" BLUE "┌─────────────────────────┐\n");
-	printf("\t│" RED " Stack size : %3i" BLUE "    │\n", info->size);
+	printf("\t│" RED "    Stack size : %3i" BLUE "     │\n", info->size);
 	printf("\t└─────────────────────────┘\n\n");
 }
